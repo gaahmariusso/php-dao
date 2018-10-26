d390e3ae92191090f4a9226eb849c15b02177d01
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2018-10-26 02:23:00 -0300

    Metodos de lista.

--- index.php
+++ index.php
@@ -2,8 +2,20 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+// Carrega um usuário
+// $root = new Usuario();
+// $root->loadById(2);
+// echo $root;
 
-$root->loadById(2);
+// Carrega uma lista de usuários
+// $lista = Usuario::getList();
+// echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuários buscando pelo login
+// $search = Usuario::search("o");
+// echo json_encode($search);
+
+// Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "12345");
+echo $usuario;
